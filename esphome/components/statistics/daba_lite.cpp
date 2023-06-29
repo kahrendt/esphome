46a3c4d031cbc216d83a177c55a004a33f3f32f2
Kevin Ahrendt <kahrendt@gmail.com>
2023-06-29 11:47:40 +0000

    use AggregateQueue in DABA Lite

--- esphome/components/statistics/daba_lite.cpp
+++ esphome/components/statistics/daba_lite.cpp
@@ -22,71 +22,8 @@ namespace statistics {
 bool DABALite::set_capacity(size_t window_size) {
   this->window_size_ = window_size;
 
-  // Mimics ESPHome's rp2040_pio_led_strip component's buf_ code (accessed June 2023)
-  ExternalRAMAllocator<float> float_allocator(ExternalRAMAllocator<float>::ALLOW_FAILURE);
-  ExternalRAMAllocator<size_t> size_t_allocator(ExternalRAMAllocator<size_t>::ALLOW_FAILURE);
-  ExternalRAMAllocator<uint32_t> uint32_t_allocator(ExternalRAMAllocator<uint32_t>::ALLOW_FAILURE);
-
-  if (this->config_.max) {
-    this->max_queue_ = float_allocator.allocate(this->window_size_);
-    if (this->max_queue_ == nullptr) {
-      return false;
-    }
-  }
-
-  if (this->config_.min) {
-    this->min_queue_ = float_allocator.allocate(this->window_size_);
-    if (this->min_queue_ == nullptr) {
-      return false;
-    }
-  }
-
-  if (this->config_.count) {
-    this->count_queue_ = size_t_allocator.allocate(this->window_size_);
-    if (this->count_queue_ == nullptr) {
-      return false;
-    }
-  }
-
-  if (this->config_.mean) {
-    this->mean_queue_ = float_allocator.allocate(this->window_size_);
-    if (this->mean_queue_ == nullptr) {
-      return false;
-    }
-  }
-
-  if (this->config_.m2) {
-    this->m2_queue_ = float_allocator.allocate(this->window_size_);
-    if (this->m2_queue_ == nullptr) {
-      return false;
-    }
-  }
-
-  if (this->config_.c2) {
-    this->c2_queue_ = float_allocator.allocate(this->window_size_);
-    if (this->c2_queue_ == nullptr) {
-      return false;
-    }
-  }
-
-  if (this->config_.timestamp_m2) {
-    this->timestamp_m2_queue_ = float_allocator.allocate(this->window_size_);
-    if (this->timestamp_m2_queue_ == nullptr) {
-      return false;
-    }
-  }
-
-  if (this->config_.timestamp_mean) {
-    this->timestamp_mean_queue_ = float_allocator.allocate(this->window_size_);
-    if (this->timestamp_mean_queue_ == nullptr) {
-      return false;
-    }
-
-    this->timestamp_reference_queue_ = uint32_t_allocator.allocate(this->window_size_);
-    if (this->timestamp_reference_queue_ == nullptr) {
-      return false;
-    }
-  }
+  if (!this->queue_.set_capacity(this->window_size_, this->config_))
+    return false;
 
   this->clear();
 
@@ -111,7 +48,7 @@ void DABALite::insert(float value) {
 
   this->back_sum_ = this->back_sum_ + lifted;
 
-  this->emplace_(lifted, this->e_.get_index());
+  this->queue_.emplace(lifted, this->e_.get_index());
 
   ++this->e_;
   ++this->size_;
@@ -136,54 +73,6 @@ Aggregate DABALite::get_current_aggregate() {
   return this->identity_class_;
 }
 
-// Store an Aggregate at specified index only in the enabled queues
-void DABALite::emplace_(const Aggregate &value, size_t index) {
-  if (this->config_.max)
-    this->max_queue_[index] = value.get_max();
-  if (this->config_.min)
-    this->min_queue_[index] = value.get_min();
-  if (this->config_.count)
-    this->count_queue_[index] = value.get_count();
-  if (this->config_.mean)
-    this->mean_queue_[index] = value.get_mean();
-  if (this->config_.m2)
-    this->m2_queue_[index] = value.get_m2();
-  if (this->config_.c2)
-    this->c2_queue_[index] = value.get_c2();
-  if (this->config_.timestamp_mean) {
-    this->timestamp_mean_queue_[index] = value.get_timestamp_mean();
-    this->timestamp_reference_queue_[index] = value.get_timestamp_reference();
-  }
-  if (this->config_.timestamp_m2)
-    this->timestamp_m2_queue_[index] = value.get_timestamp_m2();
-}
-
-// Return aggregates at a given index in the queues
-Aggregate DABALite::lower_(size_t index) {
-  Aggregate aggregate = this->identity_class_;
-
-  if (this->config_.max)
-    aggregate.set_max(this->max_queue_[index]);
-  if (this->config_.min)
-    aggregate.set_min(this->min_queue_[index]);
-  if (this->config_.count)
-    aggregate.set_count(this->count_queue_[index]);
-  if (this->config_.mean)
-    aggregate.set_mean(this->mean_queue_[index]);
-  if (this->config_.m2)
-    aggregate.set_m2(this->m2_queue_[index]);
-  if (this->config_.c2)
-    aggregate.set_c2(this->c2_queue_[index]);
-  if (this->config_.timestamp_m2)
-    aggregate.set_timestamp_m2(this->timestamp_m2_queue_[index]);
-  if (this->config_.timestamp_mean) {
-    aggregate.set_timestamp_mean(this->timestamp_mean_queue_[index]);
-    aggregate.set_timestamp_reference(this->timestamp_reference_queue_[index]);
-  }
-
-  return aggregate;
-}
-
 // DABA Lite algorithm method
 void DABALite::step_() {
   // this->debug_pointers_();
@@ -196,15 +85,15 @@ void DABALite::step_() {
       Aggregate prev_delta = this->get_delta_();
 
       --this->a_;
-      Aggregate old_a = this->lower_(this->a_.get_index());
+      Aggregate old_a = this->queue_.lower(this->a_.get_index());
 
-      this->emplace_(old_a + prev_delta, this->a_.get_index());
+      this->queue_.emplace(old_a + prev_delta, this->a_.get_index());
     }
 
     if (this->l_ != this->r_) {
-      Aggregate old_l = this->lower_(this->l_.get_index());
+      Aggregate old_l = this->queue_.lower(this->l_.get_index());
 
-      this->emplace_(old_l + this->mid_sum_, this->l_.get_index());
+      this->queue_.emplace(old_l + this->mid_sum_, this->l_.get_index());
       ++this->l_;
     } else {
       ++this->l_;
@@ -238,10 +127,10 @@ inline bool DABALite::is_front_empty_() { return (this->b_ == this->f_) && (this
 inline bool DABALite::is_delta_empty_() { return this->a_ == this->b_; }
 inline Aggregate DABALite::get_back_() { return this->back_sum_; }
 inline Aggregate DABALite::get_alpha_() {
-  return this->is_front_empty_() ? this->identity_class_ : this->lower_(this->f_.get_index());
+  return this->is_front_empty_() ? this->identity_class_ : this->queue_.lower(this->f_.get_index());
 }
 inline Aggregate DABALite::get_delta_() {
-  return this->is_delta_empty_() ? this->identity_class_ : this->lower_(this->a_.get_index());
+  return this->is_delta_empty_() ? this->identity_class_ : this->queue_.lower(this->a_.get_index());
 }
 }  // namespace statistics
 }  // namespace esphome
--- esphome/components/statistics/daba_lite.h
+++ esphome/components/statistics/daba_lite.h
@@ -32,7 +32,7 @@ class DABALite {
   // Sets the window size by adjusting the capacity of the underlying circular queues
   //  - returns whether memory was successfully allocated
   bool set_capacity(size_t window_size);
-  void set_enabled_aggregates(DABAEnabledAggregateConfiguration config) { this->config_ = config; }
+  void set_enabled_aggregates(EnabledAggregatesConfiguration config) { this->config_ = config; }
 
   // Clears all readings from the sliding window
   void clear();
@@ -49,17 +49,8 @@ class DABALite {
   Aggregate get_current_aggregate();
 
  protected:
-  size_t *count_queue_{nullptr};
-  float *max_queue_{nullptr};
-  float *min_queue_{nullptr};
-  float *mean_queue_{nullptr};
-  float *m2_queue_{nullptr};
-  float *c2_queue_{nullptr};
-  float *timestamp_m2_queue_{nullptr};
-  float *timestamp_mean_queue_{nullptr};
-  uint32_t *timestamp_reference_queue_{nullptr};
-
-  DABAEnabledAggregateConfiguration config_{};
+  AggregateQueue queue_{};
+  EnabledAggregatesConfiguration config_{};
 
   // Maximum window capacity
   size_t window_size_{0};
@@ -80,12 +71,6 @@ class DABALite {
 
   // Running aggregates for DABA Lite algorithm
   Aggregate mid_sum_, back_sum_;
-
-  // Store an Aggregate at specified index only in the enabled queues
-  void emplace_(const Aggregate &value, size_t index);
-
-  // Return aggregates at a given index in the queues
-  Aggregate lower_(size_t index);
 
   // DABA Lite algorithm method
   void step_();
