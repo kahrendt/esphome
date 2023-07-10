7e98084065429e7797b236f139ab85c525626ef2
Kevin Ahrendt <kahrendt@gmail.com>
2023-07-10 13:21:46 +0000

    improve grammar in AggregateQueue

--- esphome/components/statistics/aggregate_queue.h
+++ esphome/components/statistics/aggregate_queue.h
@@ -1,11 +1,11 @@
 /*
- * AggregateQueue class facilitates storing aggregate statistics in queues. It has three main purposes:
+ * AggregateQueue class facilitates storing aggregate statistics in queues. It has three purposes:
  *  - Allocate memory only for arrays that are necessary based on configured sensors (allocate_memory method)
  *  - Add a new Aggregate to the queue by inserting actual values into allocated arrays (emplace method)
- *  - Retreive an aggregate from the queue by reading actual values in the allocated arrays (lower method)
+ *  - Retrieve an aggregate from the queue by reading actual values in the allocated arrays (lower method)
  *
- * Additionally, it defines several virtual methods that child classes implement. This allows for uniform method calls
- * regardless of the specific queue structure used.
+ * Additionally, it defines several virtual methods that child classes implement. These virtual methods allow for
+ * uniform calls regardless of the specific child queue structure used.
  *
  * Implemented by Kevin Ahrendt for the ESPHome project, June and July 2023
  */
@@ -17,7 +17,7 @@
 namespace esphome {
 namespace statistics {
 
-/// @brief Configures which statistics will be stored in the queue
+/// @brief Configure statistics to store in the queue.
 struct EnabledAggregatesConfiguration {
   bool c2{false};
   bool duration{false};
@@ -46,10 +46,10 @@ class AggregateQueue {
   /// @brief Remove the oldest Aggregate in the queue (if sliding window) or clear it (if continuous).
   virtual void evict() = 0;
 
-  /// @brief Insert aggregate at end of queue.
+  /// @brief Insert Aggregate at the end of the queue.
   virtual void insert(Aggregate value) = 0;
 
-  /// @brief Return the aggregate summarizing all entries in the queue.
+  /// @brief Return the Aggregate summarizing all entries in the queue.
   virtual Aggregate compute_current_aggregate() = 0;
 
   /////////////////////////////
@@ -58,33 +58,33 @@ class AggregateQueue {
 
   /** Store an aggregate at an index in the queue.
    *
-   * Only the configured statistics are stored, the rest are ignored.
-   * @param value the aggregate to be emplaced
+   * Only the configured statistics are stored; the rest are ignored.
+   * @param value the Aggregate to emplace
    * @param index the index location in the queue to emplace <value>
    */
   void emplace(const Aggregate &value, size_t index);
 
-  /** Retrieve the aggregate from a particular index in the queue.
+  /** Retrieve the Aggregate from a particular index in the queue.
    *
-   * Only the configured statistics are retreived, the rest are default values.
+   * Only the configured statistics are retrieved; the rest are default values.
    * @param index the index location in the queue to retrieve an aggregate
    * @return the aggregate from <index>
    */
   Aggregate lower(size_t index);
 
-  /** Allocate memory for queue.
+  /** Allocate memory for the queue.
    *
    * Only allocates memory for statistics set in <config>. Attempts to allocate in ExternalRAM if present, otherwise it
    * falls back to built-in memory.
-   * @param capacity the number of aggregates that can be stored in the queue
-   * @param config which summary statistics should be saved into the queue
-   * @return true if memory was sucecessfuly allocated, false otherwise
+   * @param capacity the maximum number of Aggregates to store in the queue
+   * @param config which summary statistics to save into the queue
+   * @return true if the memory allocation was successful, false otherwise
    */
   bool allocate_memory(size_t capacity, EnabledAggregatesConfiguration config);
 
-  /** Return the number of aggregates inserted into queue.
+  /** Return the number of aggregates inserted into the queue.
    *
-   * @return amount of aggregates inserted and aggregated into queue
+   * @return amount of aggregates inserted and aggregated into the queue
    */
   size_t size() const { return this->size_; };
 
@@ -92,7 +92,7 @@ class AggregateQueue {
   void enable_time_weighted() { this->time_weighted_ = true; }
 
  protected:
-  // Determines whether measurements hould be weighted by duration (true) or be of equal weight (false)
+  // Determines whether measurements are weighted by duration (true) or be of equal weight (false)
   bool time_weighted_{false};
 
   // Stores the number of aggregates inserted into the queue
@@ -104,7 +104,7 @@ class AggregateQueue {
   size_t *duration_squared_queue_{nullptr};
 
   // By experimentation, using doubles for these improves accuracy in a measurable way since more computations are
-  // needed before returning a usueful statistic
+  // needed before returning a useful statistic
   double *c2_queue_{nullptr};
   double *m2_queue_{nullptr};
   double *timestamp_m2_queue_{nullptr};
@@ -113,7 +113,7 @@ class AggregateQueue {
   // Timestamps from millis() are always type uint32_t
   uint32_t *timestamp_reference_queue_{nullptr};
 
-  // FLoating point precision is only needed here, as that is what sensor->publish has as input
+  // Floating point precision is only needed here, as that is what sensor->publish has as input
   float *max_queue_{nullptr};
   float *mean_queue_{nullptr};
   float *min_queue_{nullptr};
