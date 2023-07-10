fafaae3742586b6564ad1849dcb3e74587c51282
Kevin Ahrendt <kahrendt@gmail.com>
2023-07-10 17:36:17 +0000

    Improve ContinuousQueue documentation

--- esphome/components/statistics/continuous_queue.cpp
+++ esphome/components/statistics/continuous_queue.cpp
@@ -12,7 +12,7 @@ namespace statistics {
 Aggregate ContinuousQueue::compute_current_aggregate() {
   Aggregate total = Aggregate();
 
-  // starts with the most recent aggregates so that the combine steps have as close to equal weights as possible
+  // Starts with the most recent Aggregates so that the combine operations have as close to equal weights as possible
   //  - helps avoid floating point precision issues
   for (int i = this->index_ - 1; i >= 0; --i) {
     total = total.combine_with(this->lower(i), this->time_weighted_);
@@ -22,34 +22,34 @@ Aggregate ContinuousQueue::compute_current_aggregate() {
 }
 
 void ContinuousQueue::clear() {
-  this->index_ = 0;  // resets the index
+  this->index_ = 0;  // reset the index
   this->size_ = 0;   // reset the count of aggregate chunks stored
 }
 
 void ContinuousQueue::insert(Aggregate value) {
   Aggregate most_recent = this->get_end_();
 
-  // If the most recently stored aggregate has less than or equal to the number of aggregates in value, we
+  // If the most recently stored Aggregate has less than or equal to the number of Aggregates in value, we
   // consolidate
   while ((this->index_ > 0) && (most_recent.get_count() <= value.get_count())) {
-    // combine value with most_recent
+    // Combine value with most_recent
     value = value.combine_with(most_recent, this->time_weighted_);
 
-    // store the next most_recent aggregate in the queue in most_recent
+    // Store the next most recent Aggregate in the queue in most_recent
     --this->index_;
     most_recent = this->get_end_();
   }
 
-  // If the queue is full, consolidate everything in the queue into one aggregate, even if the counts do not dictate to
-  // do so. If repeatedly done, then numerical stability can be lost. Set the capacity larger to avoid this overflow
-  // handling.
+  // If the queue is full, consolidate every Aggregate in the queue into a single Aggregate, even if the counts do not
+  // dictate to do so. If repeatedly done, then numerical stability is potentially an issue. Set the capacity larger to
+  // avoid this overflow handling.
   if (this->index_ == this->max_index_) {
     Aggregate total = this->compute_current_aggregate();
     this->emplace(total, 0);
     this->index_ = 1;
   }
 
-  // Store the new aggregate (which may have been combined with previous ones)
+  // Store the new Aggregate (which may have been combined with previous ones)
   this->emplace(value, this->index_);
 
   ++this->index_;  // increase index for next insertion
@@ -64,7 +64,7 @@ bool ContinuousQueue::set_capacity(size_t chunk_capacity, EnabledAggregatesConfi
   if (!this->allocate_memory(queue_capacity, enabled_config))
     return false;
 
-  this->max_index_ = queue_capacity;  // largest index before running out of allocated memory
+  this->max_index_ = queue_capacity;
 
   this->clear();
 
