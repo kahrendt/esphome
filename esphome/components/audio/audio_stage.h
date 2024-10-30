0e019dbb5121c34f1580ad499bb2c3b6586ccdb1
Kevin Ahrendt <kevin.ahrendt@nabucasa.com>
2024-10-30 12:44:06 +0000

    first pass at generalizing the audio stages

---
+++ esphome/components/audio/audio_stage.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "esphome/core/ring_buffer.h"
+
+namespace esphome {
+namespace audio {
+
+class AudioInputStage {
+ public:
+  AudioInputStage(esphome::RingBuffer *input_ring_buffer) : input_ring_buffer_(input_ring_buffer) {}
+
+ protected:
+  esphome::RingBuffer *input_ring_buffer_;
+};
+
+class AudioOutputStage {
+ public:
+  AudioOutputStage(esphome::RingBuffer *output_ring_buffer) : input_ring_buffer_(output_ring_buffer) {}
+
+ protected:
+  esphome::RingBuffer *output_ring_buffer_;
+};
+}  // namespace audio
+}  // namespace esphome
