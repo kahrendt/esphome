0b6d53339cdbf0a6a754f83680e2bd49fcd172de
Kevin Ahrendt <kevin.ahrendt@nabucasa.com>
2024-10-30 18:04:29 +0000

    add AudioBuffer class that may or may not be useful

--- esphome/components/audio/audio_stage.h
+++ esphome/components/audio/audio_stage.h
@@ -12,27 +12,109 @@
 namespace esphome {
 namespace audio {
 
-// class AudioBuffer {
-//  protected:
-//   esp_err_t allocate_buffer_(uint8_t **buffer, size_t buffer_size) {
-//     ExternalRAMAllocator<uint8_t> allocator(ExternalRAMAllocator<uint8_t>::ALLOW_FAILURE);
-//     if (*buffer == nullptr)
-//       *buffer = allocator.allocate(buffer_size);
+class AudioBuffer {
+ public:
+  AudioBuffer(std::shared_ptr<RingBuffer> &ring_buffer, size_t buffer_size)
+      : ring_buffer_(ring_buffer), buffer_size_(buffer_size) {
+    this->allocate_buffer_();
+  }
+  AudioBuffer(size_t ring_buffer_size, size_t buffer_size) : buffer_size_(buffer_size) {
+    this->ring_buffer_ = std::move(RingBuffer::create(ring_buffer_size));
+    this->allocate_buffer_();
+  }
+  ~AudioBuffer() {
+    if (this->buffer_ != nullptr) {
+      ExternalRAMAllocator<uint8_t> allocator(ExternalRAMAllocator<uint8_t>::ALLOW_FAILURE);
+      allocator.deallocate(this->buffer_, this->buffer_size_);
+    }
+    // Release ownership of ring buffer
+    this->ring_buffer_.reset();
+  }
 
-//     if (*buffer == nullptr)
-//       return ESP_ERR_NO_MEM;
+  size_t read_ring_buffer_(TickType_t ticks_to_wait) {
+    if (!this->allocated_successfully()) {
+      return 0;
+    }
 
-//     return ESP_OK;
-//   }
+    // Shift data in buffer to start
+    if (this->buffer_length_ > 0) {
+      memmove(this->buffer_, this->buffer_current_, this->buffer_length_);
+    }
 
-//   void deallocate_buffer_(uint8_t **buffer, size_t buffer_size) {
-//     if (*buffer != nullptr) {
-//       ExternalRAMAllocator<uint8_t> allocator(ExternalRAMAllocator<uint8_t>::ALLOW_FAILURE);
-//       allocator.deallocate(*buffer, buffer_size);
-//       *buffer = nullptr;
-//     }
-//   }
-// };
+    this->buffer_current_ = this->buffer_;
+
+    size_t bytes_read = 0;
+
+    size_t bytes_to_read = this->buffer_size_ - this->buffer_length_;
+
+    if (bytes_to_read > 0) {
+      uint8_t *new_data_start = this->buffer_ + this->buffer_length_;
+      bytes_read = this->ring_buffer_->read((void *) new_data_start, bytes_to_read, ticks_to_wait);
+
+      this->buffer_length_ += bytes_read;
+    }
+    return bytes_read;
+  }
+
+  size_t write_ring_buffer_(TickType_t ticks_to_wait) {
+    if (!this->allocated_successfully()) {
+      return 0;
+    }
+
+    size_t bytes_written = 0;
+    if (this->buffer_length_ > 0) {
+      bytes_written =
+          this->ring_buffer_->write_without_replacement((void *) this->buffer_, this->buffer_length_, ticks_to_wait);
+      this->buffer_length_ -= bytes_written;
+
+      // Shift unwritten data to the start of the buffer
+      memmove(this->buffer_, this->buffer_ + bytes_written, this->buffer_length_);
+      this->buffer_current_ = this->buffer_ + this->buffer_length_;
+    }
+    return bytes_written;
+  }
+
+  bool allocated_successfully() {
+    if (this->ring_buffer_.use_count() && (this->buffer_ != nullptr)) {
+      return true;
+    }
+    return false;
+  }
+
+  uint8_t *get_current_buffer() { return this->buffer_current_; }
+  void offset_buffer_length(size_t bytes) { this->buffer_length_ += bytes; }
+
+  size_t available() { return this->buffer_length_; }
+  size_t free() { return this->buffer_size_ - this->buffer_length_; }
+
+  std::shared_ptr<RingBuffer> &get_ring_buffer() { return this->ring_buffer_; }
+
+ protected:
+  void allocate_buffer_() {
+    ExternalRAMAllocator<uint8_t> allocator(ExternalRAMAllocator<uint8_t>::ALLOW_FAILURE);
+    this->buffer_ = allocator.allocate(this->buffer_size_);
+    this->buffer_current_ = this->buffer_;
+    this->buffer_length_ = 0;
+  }
+  uint8_t *buffer_{nullptr};
+  uint8_t *buffer_current_{nullptr};
+
+  std::shared_ptr<RingBuffer> ring_buffer_;
+
+  size_t buffer_size_;
+  size_t buffer_length_;
+};
+
+class AudioOutputBufferStage {
+ public:
+  AudioOutputBufferStage(std::shared_ptr<RingBuffer> &output_ring_buffer, size_t buffer_size) {
+    this->output_audio_buffer_ = new AudioBuffer(output_ring_buffer, buffer_size);
+  }
+  ~AudioOutputBufferStage() { delete this->output_audio_buffer_; }
+
+ protected:
+  AudioBuffer *output_audio_buffer_;
+};
 
 /******************************************************************* */
 class AudioStage {
