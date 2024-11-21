978a7c8ea16d33952d829767c0bab9ee01076098
Kevin Ahrendt <kevin.ahrendt@nabucasa.com>
2024-11-21 16:27:16 +0000

    fix bad rebase

--- esphome/components/i2s_audio/speaker/i2s_audio_speaker.h
+++ esphome/components/i2s_audio/speaker/i2s_audio_speaker.h
@@ -113,7 +113,9 @@ class I2SAudioSpeaker : public I2SAudioOut, public speaker::Speaker, public Comp
   QueueHandle_t i2s_event_queue_;
 
   uint8_t *data_buffer_;
-  std::unique_ptr<RingBuffer> audio_ring_buffer_;
+  std::shared_ptr<RingBuffer> audio_ring_buffer_;
+
+  uint32_t buffer_duration_ms_;
 
   optional<uint32_t> timeout_;
   uint8_t dout_pin_;
