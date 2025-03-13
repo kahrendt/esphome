1b0409f20dcf4932feb0f125f8d3d562a8f9e1ec
Kevin Ahrendt <kevin.ahrendt@nabucasa.com>
2025-03-13 12:39:33 +0000

    use psram for json again

--- esphome/components/json/json_util.cpp
+++ esphome/components/json/json_util.cpp
@@ -14,16 +14,17 @@ static std::vector<char> global_json_build_buffer;  // NOLINT
 static auto ALLOCATOR = RAMAllocator<uint8_t>(
     RAMAllocator<uint8_t>::NONE);  // Attempt to allocate in PSRAM before falling back into internal
 
-// struct SpiRamAllocator : ArduinoJson::Allocator {
-//   void *allocate(size_t size) { return ALLOCATOR.allocate(size); }
+struct SpiRamAllocator : ArduinoJson::Allocator {
+  void *allocate(size_t size) { return ALLOCATOR.allocate(size); }
 
-//   void deallocate(void *pointer) {
-//     free(pointer);
-//   }  // NOLINT(cppcoreguidelines-owning-memory,cppcoreguidelines-no-malloc)
+  void deallocate(void *pointer) {
+    free(pointer);
+  }  // NOLINT(cppcoreguidelines-owning-memory,cppcoreguidelines-no-malloc)
 
-//   void *reallocate(void *ptr, size_t new_size) { return ALLOCATOR.reallocate(static_cast<uint8_t *>(ptr), new_size);
-//   }
-// };
+  void *reallocate(void *ptr, size_t new_size) { return ALLOCATOR.reallocate(static_cast<uint8_t *>(ptr), new_size); }
+};
+
+static auto DOC_ALLOCATOR = SpiRamAllocator();
 
 // static const auto SpiRamAllocator JSON_ALLOCATOR;
 // using SpiRamJsonDocument = BasicJsonDocument<SpiRamAllocator>;
@@ -39,7 +40,7 @@ std::string build_json(const json_build_t &f) {
     ESP_LOGV(TAG, "Attempting to allocate %zu bytes for JSON serialization", request_size);
     // DynamicJsonDocument json_document(request_size);
     // SpiRamJsonDocument json_document(request_size);
-    JsonDocument json_document;
+    JsonDocument json_document(&DOC_ALLOCATOR);
     if (json_document.overflowed()) {
       ESP_LOGE(TAG, "Could not allocate memory for JSON document!");
       return "{}";
@@ -66,7 +67,7 @@ bool parse_json(const std::string &data, const json_parse_t &f) {
   // auto free_heap = ALLOCATOR.get_max_free_block_size();
   // size_t request_size = std::min(free_heap, (size_t) (data.size() * 1.5));
   while (true) {
-    JsonDocument json_document;
+    JsonDocument json_document(&DOC_ALLOCATOR);
     // DynamicJsonDocument json_document(request_size);
     // SpiRamJsonDocument json_document(request_size);
     if (json_document.overflowed()) {
