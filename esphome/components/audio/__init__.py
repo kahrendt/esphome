04c306c77f2115a934489f3ec5f41c6299186bee
Kevin Ahrendt <kevin.ahrendt@openhomefoundation.org>
2026-02-12 10:54:46 -0500

    don't use include_builtin_idf_component so we can still build on 2026.1.5

--- esphome/components/audio/__init__.py
+++ esphome/components/audio/__init__.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 import esphome.codegen as cg
-from esphome.components.esp32 import add_idf_component, include_builtin_idf_component
+from esphome.components.esp32 import add_idf_component
 import esphome.config_validation as cv
 from esphome.const import CONF_BITS_PER_SAMPLE, CONF_NUM_CHANNELS, CONF_SAMPLE_RATE
 from esphome.core import CORE
@@ -200,7 +200,7 @@ def final_validate_audio_schema(
 
 async def to_code(config):
     # Re-enable ESP-IDF's HTTP client (excluded by default to save compile time)
-    include_builtin_idf_component("esp_http_client")
+    # include_builtin_idf_component("esp_http_client")
 
     add_idf_component(
         name="esphome/esp-audio-libs",
